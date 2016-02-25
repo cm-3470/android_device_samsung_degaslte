94e084e7f0ddaa4ba2020ace184e12dee773cb79
CTXz <ctx.xda@gmail.com>
2016-02-25 19:17:06 +0100

    Degaslte: Use full path instead LOCAL_PATH

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,4 @@
-LOCAL_PATH := device/samsung/degaslte
-
 PRODUCT_MAKEFILES := \
-	$(LOCAL_PATH)omni_degaslte.mk \
-	$(LOCAL_PATH)full_degaslte.mk
+	device/samsung/degaslte/omni_degaslte.mk
+	device/samsung/degaslte/full_degaslte.mk
+
